582955c52e9dac9a71d6171059df547785dd3c4e
Irok3Zz <75535405+Irok3Zz@users.noreply.github.com>
2021-04-11 11:57:46 +0300

    Add files via upload

---
+++ serverSticksy/AnswerMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SticksyProtocol;
+
+namespace AnswerMethods
+{
+    static class AnswerMethods
+    {
+        static public AnswerId SignUp() // Регистрация
+        {
+            AnswerId answerId = new AnswerId();
+
+            /* To Do
+             answerId = GetIdFormDB(); // Возвращает id нового User или -1, если такой пользователь уже занят
+            */
+
+            return answerId;
+        }
+        static public AnswerUser SignIn() // Авторизация
+        {
+            AnswerUser answerUser = new AnswerUser();
+
+            /* To Do 
+            answerUser.User = GetUserFromDB(); // Возвращает объект User или null, если нет совпадений в базе
+            */
+
+            return answerUser;
+        }
+
+    }
+}
---
+++ serverSticksy/SticksyProtocol/Protocol.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SticksyProtocol
+{
+    public interface Data { }
+
+    [Serializable]
+    public class SignUp:Data
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+
+    [Serializable]
+    public class SignIn : Data
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+
+    [Serializable]
+    public class CreateStick : Data
+    {
+        public int idCreator { get; set; }       
+    }
+
+    [Serializable]
+    public class DelStick : Data
+    {
+        public int idStick { get; set; }
+    }
+
+    [Serializable]
+    public class EditStick : Data
+    {
+        public Stick stick { get; set; }
+    }
+
+    [Serializable]
+    public class GetUsers : Data { }
+
+    [Serializable]
+    public class AddFriend : Data
+    {
+        public int idCreator { get; set; }
+        public int idFriend { get; set; }
+        public int idStick { get; set; }
+    }
+
+    [Serializable]
+    public class AnswerId : Data
+    {
+        public int id { get; set; }
+    }
+
+    [Serializable]
+    public class AnswerUser : Data
+    {
+        public User user { get; set; }
+    }
+
+    [Serializable]
+    public class AnswerListUser : Data
+    {
+        public List<Friend> users { get; set; }
+    }
+    public class Transfer
+    {
+        private static BinaryFormatter formatter = new BinaryFormatter();
+        public static void SendData(TcpClient clientSocket, Data data)
+        {
+            formatter.Serialize(clientSocket.GetStream(), data);
+        }
+
+        public static async void SendDataAsync(TcpClient clientSocket, Data data)
+        {
+            await Task.Run(() => formatter.Serialize(clientSocket.GetStream(), data));
+        }
+
+        public static Data ReceiveData(TcpClient clientSocket)
+        {
+            return (Data)formatter.Deserialize(clientSocket.GetStream());
+        }
+    }
+}
