06e9e09c7144696bb5c806ef17e835f5eb7003b1
Irok3Zz <75535405+Irok3Zz@users.noreply.github.com>
2021-04-13 13:18:41 +0300

    Методы на SignUp/SignIn

--- serverSticksy/AnswerMethods.cs
+++ serverSticksy/AnswerMethods.cs
@@ -4,30 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SticksyProtocol;
+using DataBaseProtocol;
 
 namespace AnswerMethods
 {
     static class AnswerMethods
     {
-        static public AnswerId SignUp() // Регистрация
+        static public AnswerId SignUp(Sign newUser) // SignUp(Регистрация)
         {
-            AnswerId answerId = new AnswerId();
-
-            /* To Do
-             answerId = GetIdFormDB(); // Возвращает id нового User или -1, если такой пользователь уже занят
-            */
-
-            return answerId;
+            // Возвращает id нового User или -1, если такой пользователь уже занят
+            return new AnswerId(SaveBase.SaveUserRegistration(new User(0, newUser.login, newUser.password)));
         }
-        static public AnswerUser SignIn() // Авторизация
+        static public AnswerUser SignIn(Sign checkUser) // SignIn(Авторизация)
         {
-            AnswerUser answerUser = new AnswerUser();
-
-            /* To Do 
-            answerUser.User = GetUserFromDB(); // Возвращает объект User или null, если нет совпадений в базе
-            */
-
-            return answerUser;
+            // Возвращает объект User или null, если нет совпадений в базе
+            return new AnswerUser(QueryBase.CheckUserIn(new User(0, checkUser.login, checkUser.password)));
         }
 
     }
