3f20c5560dca433f435cfecdd27438eb3df1ca51
Tymur <66021340+TNiurhechev@users.noreply.github.com>
2021-04-15 18:53:55 +0300

    Add files via upload
    create/edit/delete stick

---
+++ serverSticksy/StickServerIntegration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SticksyProtocol;
+using BaseProtocol;
+
+namespace StickServerIntegration
+{
+     class StickServerIntegration
+    {
+        StiksyDataBase DBquery;
+        StickServerIntegration(StiksyDataBase constr)
+        {
+            DBquery = constr;
+        }
+        public AnswerId CreateStick(int creatorId)
+        {
+            return new AnswerId(DBquery.CreateStick(creatorId),TypeId.IdStick);
+        }
+        public void EditStick(Stick stick)
+        {
+            DBquery.UpdateStick(stick);
+        }
+        public void DeleteStick(int stickId)
+        {
+            DBquery.DeleteStick(stickId);
+        }
+    }
+}
